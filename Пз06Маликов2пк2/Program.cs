e2a2c2c1589f1683672842985a7a12631e467171
Malikov Egor <egor122malikov@gmail.com>
2024-11-05 11:57:43 +0300

    first commit

---
+++ Пз06Маликов2пк2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Пз06Маликов2пк2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
